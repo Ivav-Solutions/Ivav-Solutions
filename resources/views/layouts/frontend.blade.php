895b1d40ecab0637c6d98d21a33bb88ba1e3a56e
Promise <promiseezema11@gmail.com>
2022-08-05 09:46:57 +0100

    updated

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -35,7 +35,7 @@
                 $(".alert-timeout").fadeTo(500, 0).slideUp(1000, function(){
                     $(this).remove(); 
                 });
-            }, 7000);
+            }, 11000);
         </script>
     </head>
 
